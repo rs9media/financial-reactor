38cd701520d01f8c1315c4e4a061a410859bcc72
RS9 Media <50057755+rs9media@users.noreply.github.com>
2019-07-14 14:09:17 -0700

    Update for embedding into ui harness

--- core.js
+++ core.js
@@ -8,17 +8,20 @@ var Ring = function(configuration) {
     self.generators = [];
     //Outputs
     self.loads = [];
+    //Avatar
+    self.avatar = null;
     //Frequency
-    self.frequency = 0;    
+    self.frequency = 0;
     //Generator Sum
     self.generatorSum = 0;
     //Input Requirement
     self.generatorRequirement = 0;
     //Constructor
     self.constructor = function() {
-        self.label = configuration.label;        
+        self.label = configuration.label;
         self.generators = configuration.generators;
         self.loads = configuration.loads;
+        self.avatar = self.renderRing();
         setInterval(self.loop, 3000);
     };
     //Loop
@@ -29,50 +32,99 @@ var Ring = function(configuration) {
         self.calculateGeneratorRequirements();
         //Calculate Frequency
         self.calculateFrequency();
-        
         //Log Data
-        console.log(self.label,{
-            "frequency":self.frequency,
-            "generatorSum":self.generatorSum,
-            "generatorRequirement":self.generatorRequirement
+        console.log(self.label, {
+            "frequency": self.frequency,
+            "generatorSum": self.generatorSum,
+            "generatorRequirement": self.generatorRequirement
         });
-        
+        //Set Ring Spin
+        self.setRingSpin(self.frequency);
         //Fault Warning
-        if(self.generatorSum < self.generatorRequirement){
+        if (self.generatorSum < self.generatorRequirement) {
             throw {
-                "instance":self.label,
-                "name" : "LOW_GENERATOR_CURRENT",
-                "message" : "Financial current is too low to spin this Ring"
+                "instance": self.label,
+                "name": "LOW_GENERATOR_CURRENT",
+                "message": "Financial current is too low to spin this Ring"
             };
         }
-    }; 
+    };
+    //Render Ring
+    self.renderRing = function() {
+        //Ring
+        var ring = $("<img>").addClass("fa-spin").attr("src", "http://clipart-library.com/images_k/transparent-gears/transparent-gears-8.png");
+        //Add ID
+        $(ring).attr("identifier", self.label);
+        //Calcualate Size	  
+        $(ring).width((180 + (8 * self.loads.length)) + "px");
+        //Add to #stage
+        $("#stage").append(ring);
+        //Break;
+        $("#stage").append("</br>");
+        //Show what's in it
+        $("#stage").append(
+            $("<span>").text(self.renderLoadList())
+        );
+        //Break;
+        $("#stage").append("</br>");				
+        //Return a handle
+        return ring;
+    };
+    //Render Load List
+    self.renderLoadList = function(){
+        //Load List
+        var loadList = [];
+        //Loop through loads
+        for (i in self.loads) {
+            //Current Output
+            var currentLoad = self.loads[i];
+            //Add to list
+            loadList.push(currentLoad.label);
+        }	
+        //Join and send back
+        return loadList.join(" :: ");
+    };
+    //Set Ring Spin
+    self.setRingSpin = function(spin) {
+        //Rate
+        var rate = null;
+        //If we have a value, use it
+        if(spin > 0){
+            rate = (1 / spin);
+        }
+        else{
+            rate = 0;
+        }
+        //Spin Label
+        $(self.avatar).attr("spinRate", spin);
+        $(self.avatar).css("animation", "fa-spin " + rate + "s infinite linear");
+    };
     //Add Load
-    self.addLoad = function(additionalLoad){
-        console.log(self.label + " add load",additionalLoad);
+    self.addLoad = function(additionalLoad) {
+        console.log(self.label + " add load", additionalLoad);
         self.loads.push(additionalLoad);
     };
     //Add Generator
-    self.addGenerator = function(additionalGenerator){
+    self.addGenerator = function(additionalGenerator) {
         self.generators.push(additionalGenerator);
     };
     //Calculate Frequency
-    self.calculateFrequency = function(){
+    self.calculateFrequency = function() {
         //Frequency
-        if(self.generatorRequirement > 0){
+        if (self.generatorRequirement > 0) {
             self.frequency = (self.generatorSum / self.generatorRequirement);
+        } else {
+            self.frequency = 0;
         }
-        else{            
-            self.frequency = 0;        
-        }        
         //Send it back
         return self.frequency;
     };
     //Calculate Generator Sum
-    self.calculateGeneratorSum = function(){
+    self.calculateGeneratorSum = function() {
         //Generator Sum
-        self.generatorSum = 0;    
+        self.generatorSum = 0;
         //Loop through generators
-        for(i in self.generators){
+        for (i in self.generators) {
             //Current Generator Object
             var currentGeneratorObject = self.generators[i];
             //Increment generatorSum
@@ -82,11 +134,11 @@ var Ring = function(configuration) {
         return self.generatorSum;
     };
     //Calculate Input Requirements
-    self.calculateGeneratorRequirements = function(){
+    self.calculateGeneratorRequirements = function() {
         //Input Requirement
-        self.generatorRequirement = 0;    
+        self.generatorRequirement = 0;
         //Loop through loads
-        for(i in self.loads){
+        for (i in self.loads) {
             //Current Output
             var currentOutputObject = self.loads[i];
             //Increment input requirement
@@ -96,7 +148,7 @@ var Ring = function(configuration) {
         return self.generatorRequirement;
     };
     //Calculate Surplus Current
-    self.calculateSurplusCurrent = function(){
+    self.calculateSurplusCurrent = function() {
         return (self.calculateGeneratorSum() - self.calculateGeneratorRequirements());
     }
     //Showtime
@@ -135,7 +187,7 @@ var FinancialCurrentLoad = function(configuration) {
     self.constructor();
 };
 //Financial Reactor
-var FinancialReactor = function() {
+var FinancialReactor = function(rings) {
     //Internal Reference
     var self = this;
     //Rings
@@ -143,122 +195,24 @@ var FinancialReactor = function() {
     //Constructor
     self.constructor = function() {
         //Rings
-        self.rings = {
-            "L1": new Ring({
-                "label":"L1",
-                "generators": [
-                    new FinancialCurrentGenerator({
-                        "label": "Uber Eats",
-                        "amount": 30
-                    })
-                ],
-                "loads": [
-                    new FinancialCurrentLoad({
-                        "label": "Operator",
-                        "amount": 7
-                    }),
-                    new FinancialCurrentLoad({
-                        "label": "GBV/ARES-5",
-                        "amount": 7
-                    }),
-                    new FinancialCurrentLoad({
-                        "label": "GBV/ARES-5 Insurance",
-                        "amount": 2.41
-                    }),
-                    new FinancialCurrentLoad({
-                        "label": "Comm Package",
-                        "amount": 1.61
-                    })
-                ]
-            }),
-            "L2": new Ring({
-                "label":"L2",                
-                "generators": [
-                    new FinancialCurrentGenerator({
-                        "label": "Type-W Development Contracts",
-                        "amount": 0
-                    }),
-                    new FinancialCurrentGenerator({
-                        "label": "Type-W Maintenance Contracts",
-                        "amount": 2
-                    })       
-                ],
-                "loads": [
-                    new FinancialCurrentLoad({
-                        "label": "L1 Ring",
-                        "amount": 30
-                    }),
-                    new FinancialCurrentLoad({
-                        "label": "Operator",
-                        "amount": 7
-                    }),                    
-                    new FinancialCurrentLoad({
-                        "label": "MWS/FA-300",
-                        "amount": 20
-                    })
-                ]
-            }),
-            "L3": new Ring({
-                "label":"L3",                
-                "generators": [
-                    new FinancialCurrentGenerator({
-                        "label": "Type-W Development Contracts",
-                        "amount": 0
-                    }),
-                    new FinancialCurrentGenerator({
-                        "label": "Type-W Maintenance Contracts",
-                        "amount": 0
-                    }),
-                    new FinancialCurrentGenerator({
-                        "label": "Type-R Sales Profits",
-                        "amount": 0
-                    }),
-                    new FinancialCurrentGenerator({
-                        "label": "Type-P Subscriptions",
-                        "amount": 0
-                    })      
-                ],
-                "loads": [
-                    new FinancialCurrentLoad({
-                        "label": "L2 Ring",
-                        "amount": 80
-                    }),
-                    new FinancialCurrentLoad({
-                        "label": "Operator",
-                        "amount": 7
-                    }),                    
-                    new FinancialCurrentLoad({
-                        "label": "MWS/FA-300",
-                        "amount": 20
-                    }),
-                    new FinancialCurrentLoad({
-                        "label": "Data / Engineer / Artisan  Staff",
-                        "amount": 13
-                    })
-                ]
-            })          
-        };
+        self.rings = rings;
         //Loop through each ring...
-        for(identifier in self.rings){
+        for (identifier in self.rings) {
             //Calculate Surplus Current
             var currentRingSurplusCurrent = self.rings[identifier].calculateSurplusCurrent();
             //Lel
-            console.log(self.rings[identifier].label + " LEL SURPLUS",currentRingSurplusCurrent);
+            console.log(self.rings[identifier].label + " LEL SURPLUS", currentRingSurplusCurrent);
             //If there is a surplus, harvest the current
-            if(currentRingSurplusCurrent > 0){
+            if (currentRingSurplusCurrent > 0) {
                 self.rings[identifier].addLoad(
                     new FinancialCurrentLoad({
                         "label": "Warchest",
                         "amount": currentRingSurplusCurrent
                     })
                 );
-            }    
+            }
         }
-        
-        
     }
     //Showtime
     self.constructor();
 };
-
-new FinancialReactor();
